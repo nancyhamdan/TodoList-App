5369f32d004148e5f4089d6abfb019adde49bd15
Nancy Hamdan <nancyehamdan@gmail.com>
2021-06-13 16:54:45 +0400

    deleted handlers folder and added indexHandler to main

--- main.go
+++ main.go
@@ -1,9 +1,24 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
+	"net/http"
 )
 
+const (
+	PORT = "8080"
+)
+
+var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+
+func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	tmpl.Execute(w, nil)
+}
+
 func main() {
-	fmt.Println("Hello World")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/", IndexHandler)
+
+	http.ListenAndServe(":"+PORT, mux)
 }
