374c0ec0920158a525420df0844bb80349710ca5
Nancy Hamdan <nancyehamdan@gmail.com>
2021-07-15 16:53:39 +0300

    changed project structure to use src, pkg and web folders

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"net/http"
 	"os"
 )
 
-var tmpl = template.Must(template.ParseFiles("templates/index.html"))
+var tmpl = template.Must(template.ParseFiles("../web/templates/index.html"))
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
@@ -17,11 +18,12 @@ func main() {
 	if port == "" {
 		port = "3000"
 	}
+	fmt.Println("running on port:", port)
 
 	mux := http.NewServeMux()
-	stylesServer := http.FileServer(http.Dir("static/styles"))
+	stylesServer := http.FileServer(http.Dir("../web/static/styles"))
 
 	mux.HandleFunc("/", IndexHandler)
-	mux.Handle("/static/styles/", http.StripPrefix("/static/styles/", stylesServer))
+	mux.Handle("/static/styles/", http.StripPrefix("/static/styles", stylesServer))
 	http.ListenAndServe(":"+port, mux)
 }
