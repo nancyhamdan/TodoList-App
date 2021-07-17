698dd1f7fdfcdc3afe25d8ab9dab9a4852d26805
Nancy Hamdan <nancyehamdan@gmail.com>
2021-07-18 02:48:47 +0300

    js file for common functionalities

---
+++ web/static/scripts/common.js
@@ -0,0 +1,9 @@
+function showAddTaskPopUp() {
+    let modals = document.getElementsByClassName("modal");
+    modals[0].style.display = "flex";
+}
+
+function hideAddTaskPopUp() {
+    let modals = document.getElementsByClassName("modal");
+    modals[0].style.display = "none";
+}
