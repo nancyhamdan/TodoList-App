8703b91f27b868a179b314734bb8cf6714e06617
Nancy Hamdan <nancyehamdan@gmail.com>
2021-07-18 04:02:49 +0300

    started the select task functionality

--- web/static/scripts/common.js
+++ web/static/scripts/common.js
@@ -7,3 +7,25 @@ function hideAddTaskPopUp() {
     let modals = document.getElementsByClassName("modal");
     modals[0].style.display = "none";
 }
+
+let selectedTask;
+
+let tasks = document.getElementsByClassName("task-container");
+for (let i = 0; i < tasks.length; i++) {
+    tasks[i].addEventListener("click", selectTask);
+}
+
+function selectTask() {
+    console.log("inside selectTask");
+    this.style.border = "thick solid rgb(212, 96, 64)";
+    showButtons();
+    selectedTask = this;
+    console.log(selectedTask)
+    let desc = selectedTask.querySelector('.task-desc');
+    console.log("task desc:", desc.innerHTML);
+}
+
+function showButtons() {
+    document.getElementById("update-btn").style.display = "block";
+    document.getElementById("delete-btn").style.display = "block";
+}
