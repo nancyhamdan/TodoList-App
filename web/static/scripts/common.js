e781b7f337b428b6f20a996d72ea998cc7011d49
Nancy Hamdan <nancyehamdan@gmail.com>
2021-07-19 05:12:17 +0300

    finished main design and dynamics

--- web/static/scripts/common.js
+++ web/static/scripts/common.js
@@ -1,12 +1,27 @@
+document.getElementById('add-btn').addEventListener('click', showAddTaskPopUp);
+document.getElementById('update-btn').addEventListener('click', showAddTaskPopUp);
+document.getElementById('delete-btn').addEventListener('click', showDeleteTaskPopUp);
+document.getElementById('add-modal-close').addEventListener('click', hideAddTaskPopUp);
+document.getElementById('delete-modal-close').addEventListener('click', hideDeleteTaskPopUp);
+document.getElementById('add-modal-cancel').addEventListener('click', hideAddTaskPopUp);
+document.getElementById('delete-modal-cancel').addEventListener('click', hideDeleteTaskPopUp);
+
 function showAddTaskPopUp() {
-    let modals = document.getElementsByClassName("modal");
-    modals[0].style.display = "flex";
+    document.getElementById('add-modal').style.display = "flex";
+}
+
+function showDeleteTaskPopUp() {
+    document.getElementById('delete-modal').style.display = "flex";
 }
 
 function hideAddTaskPopUp() {
-    let modals = document.getElementsByClassName("modal");
-    modals[0].style.display = "none";
+    document.getElementById('add-modal').style.display = "none";
 }
+
+function hideDeleteTaskPopUp() {
+    document.getElementById('delete-modal').style.display = "none";
+}
+
 
 let selectedTask;
 
@@ -16,6 +31,7 @@ for (let i = 0; i < tasks.length; i++) {
 }
 
 function selectTask() {
+    deselectOtherTasks(this);
     console.log("inside selectTask");
     this.style.border = "thick solid rgb(212, 96, 64)";
     showButtons();
@@ -25,7 +41,74 @@ function selectTask() {
     console.log("task desc:", desc.innerHTML);
 }
 
+function deselectOtherTasks(task) {
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i] != task) {
+            tasks[i].style.border = 'none';
+        }
+    }
+}
+
 function showButtons() {
     document.getElementById("update-btn").style.display = "block";
     document.getElementById("delete-btn").style.display = "block";
+}
+
+function hideButtons() {
+    document.getElementById("update-btn").style.display = "none";
+    document.getElementById("delete-btn").style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target.className.includes('task-') != true && event.target.className.includes('button') != true) {
+        for (let i = 0; i < tasks.length; i++) {
+            tasks[i].style.border = 'none';
+            hideButtons();
+        }
+    }
+
+    if (event.target.classList.contains('modal')) {
+        hideAddTaskPopUp();
+        hideDeleteTaskPopUp();
+    }
+}
+
+let completeButtons = document.getElementsByClassName("complete-circle");
+for (let i = 0; i < completeButtons.length; i++) {
+    completeButtons[i].addEventListener("click", clickCompleteTask);
+}
+
+function clickCompleteTask() {
+    let uncompletedTasks = document.getElementsByClassName("uncompleted-tasks")[0];
+    let completedTasks = document.getElementsByClassName("completed-tasks")[0];
+    if (this.classList.contains('checked')) {
+        completedTasks.removeChild(this.parentNode);
+        if (completedTasks.children.length == 1) {
+            document.getElementById('completed-div').style.display = 'none';
+        }
+        uncompletedTasks.appendChild(this.parentNode);
+
+        this.classList.replace('fa-check-circle', 'fa-circle');
+        this.classList.remove("checked");
+    } else {
+        document.getElementById('completed-div').style.display = 'flex';
+        uncompletedTasks.removeChild(this.parentNode);
+        completedTasks.appendChild(this.parentNode);
+
+        this.classList.replace('fa-circle', 'fa-check-circle');
+        this.classList.add("checked");
+    }
+}
+
+let starButtons = document.getElementsByClassName("important-star");
+for (let i = 0; i < starButtons.length; i++) {
+    starButtons[i].addEventListener("click", clickStarTask);
+}
+
+function clickStarTask() {
+    if (this.classList.contains('checked')) {
+        this.classList.remove('checked');
+    } else {
+        this.classList.add('checked');
+    }
 }
