8ff1ec47740384221b3c4b10cef244ca9a838eea
Nancy Hamdan <nancyehamdan@gmail.com>
2021-08-28 23:18:48 +0400

    layed out foundation for backend

--- pkg/models/task.go
+++ pkg/models/task.go
@@ -4,55 +4,27 @@ type Task struct {
 	ID          int
 	Description string
 	DueDate     string
+	IsToday     bool
 	IsImportant bool
 	IsCompleted bool
-	IsToday     bool
 }
 
-var tasks []*Task
+// func NewTask() (*Task, error) {
 
-func createTask(tInfo map[string]interface{}) *Task {
-	task := Task{}
-	task.ID = tInfo["ID"].(int)
-	task.Description = tInfo["Description"].(string)
-	task.DueDate = tInfo["DueDate"].(string)
-	task.IsImportant = tInfo["IsImportant"].(bool)
-	task.IsCompleted = tInfo["IsCompleted"].(bool)
-	task.IsToday = tInfo["IsToday"].(bool)
-	return &task
-}
+// }
 
-func getTask(taskId int) *Task {
-	for _, task := range tasks {
-		if task.ID == taskId {
-			return task
-		}
-	}
-	return &Task{}
-}
+// func (task *Task) UpdateTask() error {
 
-func addTask(task *Task) {
-	tasks = append(tasks, task)
-}
+// }
 
-func updateTask(taskId int, newInfo map[string]interface{}) {
-	for _, task := range tasks {
-		if task.ID == taskId {
-			task.ID = newInfo["ID"].(int)
-			task.Description = newInfo["Description"].(string)
-			task.DueDate = newInfo["DueDate"].(string)
-			task.IsImportant = newInfo["IsImportant"].(bool)
-			task.IsCompleted = newInfo["IsCompleted"].(bool)
-			task.IsToday = newInfo["IsToday"].(bool)
-		}
-	}
-}
+// func (task *Task) UpdateTaskImportance() error {
 
-func deleteTask(taskId int) {
-	for i, task := range tasks {
-		if task.ID == taskId {
-			tasks[i] = tasks[len(tasks)-1]
-			tasks = tasks[:len(tasks)-1]
-		}
-	}
-}
+// }
+
+// func (task *Task) UpdateTaskCompletion() error {
+
+// }
+
+// func (task *Task) DeleteTask() error {
+
+// }
--- pkg/models/user.go
+++ pkg/models/user.go
@@ -1,7 +1,35 @@
 package models
 
 type User struct {
+	ID       int
 	Username string
-	Email    string
 	Password string
 }
+
+// func NewUser() (*User, error) {
+
+// }
+
+// func (user *User) RegisterUser() {
+
+// }
+
+// func (user *User) Authenticate(password string) error {
+
+// }
+
+// func (user *User) GetAllTasks() ([]*Task, error) {
+
+// }
+
+// func (user *User) GetAllTodayTasks() ([]*Task, error) {
+
+// }
+
+// func (user *User) GetAllImportantTasks() ([]*Task, error) {
+
+// }
+
+// func (user *User) GetAllPlannedTasks() ([]*Task, error) {
+
+// }
