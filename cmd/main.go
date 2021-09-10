d7915cb561056ffb66b5266a3ed9d6e8c58eef50
Nancy Hamdan <nancyehamdan@gmail.com>
2021-09-10 18:39:23 +0400

    added logout endpoint

--- cmd/main.go
+++ cmd/main.go
@@ -17,15 +17,17 @@ func createRouter() *mux.Router {
 	staticFilesServer := http.FileServer(http.Dir("../web/static"))
 
 	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", staticFilesServer))
+	r.HandleFunc("/", middleware.AuthRequired(handlers.IndexHandler)).Methods("GET")
 	r.HandleFunc("/signup", handlers.SignUpGetHandler).Methods("GET")
 	r.HandleFunc("/signup", handlers.SignUpPostHandler).Methods("POST")
 	r.HandleFunc("/login", handlers.LoginGetHandler).Methods("GET")
 	r.HandleFunc("/login", handlers.LoginPostHandler).Methods("POST")
-	r.HandleFunc("/", middleware.AuthRequired(handlers.GetAllTodayTasksHandler)).Methods("GET")
+	r.HandleFunc("/logout", handlers.LogoutHandler)
 	r.HandleFunc("/tasks", middleware.AuthRequired(handlers.AddTaskHandler)).Methods("POST")
 	r.HandleFunc("/tasks/{id}", middleware.AuthRequired(handlers.UpdateTaskHandler)).Methods("PUT")
 	r.HandleFunc("/tasks/{id}", middleware.AuthRequired(handlers.DeleteTaskHandler)).Methods("DELETE")
 	r.HandleFunc("/tasks", middleware.AuthRequired(handlers.GetAllTasksHandler)).Methods("GET")
+	r.HandleFunc("/tasks/today", middleware.AuthRequired(handlers.GetAllTodayTasksHandler)).Methods("GET")
 	r.HandleFunc("/tasks/important", middleware.AuthRequired(handlers.GetAllImportantTasksHandler)).Methods("GET")
 	r.HandleFunc("/tasks/planned", middleware.AuthRequired(handlers.GetAllPlannedTasksHandler)).Methods("GET")
 
